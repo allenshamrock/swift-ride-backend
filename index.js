dd1f00d623f6ffa895816652e14bb0fb4e477bbc
Allen <allenshamrock37@gmail.com>
2024-03-28 14:56:55 +0300

    {Enhance}:Refactored index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8000
 
-server.use(middlewares)
-server.use(router)
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.use(router);
 
-server.listen(port)
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
